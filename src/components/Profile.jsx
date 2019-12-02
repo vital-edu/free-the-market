775b73d23b70bc8bd3273a7df10f4f9f25d7e56b
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2019-12-01 23:38:07 -0300

    add simple storage of status on Gaia

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,26 +4,154 @@ import NavBar from './NavBar'
 import { appConfig } from '../assets/constants'
 import '../styles/Profile.css'
 
+const avatarFallbackImage = './avatar-placeholder.png'
+
 export default class Profile extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      person: {
+        name() {
+          return 'Anonymous'
+        },
+        avatarUrl() {
+          return avatarFallbackImage
+        },
+      },
+      username: '',
+      newStatus: '',
+      statuses: [],
+      statusIndex: 0,
+      isLoading: false,
+    }
+  }
+
+  componentWillMount() {
+    const { userSession } = this.props
+    this.setState({
+      person: new Person(userSession.loadUserData().profile),
+      username: userSession.loadUserData().username
+    });
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  handleNewStatusChange(event) {
+    this.setState({ newStatus: event.target.value })
+  }
+
+  handleNewStatusSubmit(event) {
+    this.saveNewStatus(this.state.newStatus)
+    this.setState({
+      newStatus: ""
+    })
+  }
+
+  saveNewStatus(statusText) {
+    const { userSession } = this.props
+    let statuses = this.state.statuses
+
+    let status = {
+      id: this.state.statusIndex++,
+      text: statusText.trim(),
+      created_at: Date.now()
+    }
+
+    statuses.unshift(status)
+    const options = { encrypt: false }
+    userSession.putFile('statuses.json', JSON.stringify(statuses), options)
+      .then(() => {
+        this.setState({
+          statuses: statuses
+        })
+      })
+  }
+
+  fetchData() {
+    const { userSession } = this.props
+    this.setState({ isLoading: true })
+    const options = { decrypt: false }
+    userSession.getFile('statuses.json', options)
+      .then((file) => {
+        var statuses = JSON.parse(file || '[]')
+        this.setState({
+          person: new Person(userSession.loadUserData().profile),
+          username: userSession.loadUserData().username,
+          statusIndex: statuses.length,
+          statuses: statuses,
+        })
+      })
+      .finally(() => {
+        this.setState({ isLoading: false })
+      })
+  }
+
   render() {
-    const username = this.props.userSession.loadUserData().username;
-    const profile = this.props.userSession.loadUserData();
-    const person = new Person(profile);
+    const { handleSignOut, userSession } = this.props;
+    const { person } = this.state;
+    const { username } = this.state;
+
     return (
-      <div className="Dashboard">
-        <NavBar username={username} user={person} signOut={this.props.handleSignOut} />
-        <div className="row justify-content-center" id="header">
-          <h3 className="user-info">
-            {username}
-          </h3>
-        </div>
-        <br></br>
-        <div className="row justify-content-center">
-        </div>
-        <br></br>
-        <div className="row justify-content-center">
-        </div>
-      </div>
+      !userSession.isSignInPending() && person ?
+        <div className="container">
+          <NavBar username={username} user={person} signOut={this.props.handleSignOut} />
+          <div className="row">
+            <div className="col-md-offset-3 col-md-6">
+              <div className="col-md-12">
+                <div className="avatar-section">
+                  <img
+                    src={person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage}
+                    className="img-rounded avatar"
+                    id="avatar-image"
+                    alt="foto de perfil"
+                  />
+                  <div className="username">
+                    <h1>
+                      <span id="heading-name">{person.name() ? person.name()
+                        : 'Nameless Person'}</span>
+                    </h1>
+                    <span>{username}</span>
+                    <span>
+                      &nbsp;|&nbsp;
+                    <a onClick={handleSignOut.bind(this)}>(Logout)</a>
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="new-status">
+                <div className="col-md-12">
+                  <textarea className="input-status"
+                    value={this.state.newStatus}
+                    onChange={e => this.handleNewStatusChange(e)}
+                    placeholder="Enter a status"
+                  />
+                </div>
+                <div className="col-md-12">
+                  <button
+                    className="btn btn-primary btn-lg"
+                    onClick={e => this.handleNewStatusSubmit(e)}
+                  >
+                    Submit
+                </button>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <div className="col-md-12 statuses">
+            {this.state.isLoading && <span>Loading...</span>}
+            {this.state.statuses.map((status) => (
+              <div className="status" key={status.id}>
+                {status.text}
+              </div>
+            )
+            )}
+          </div>
+        </div> : null
     );
   }
 
