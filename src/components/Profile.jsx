93232f966b0bbd08dee304e0fac64b835b7e1a84
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2019-12-02 11:35:04 -0300

    use hooks on Profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   Person,
   lookupProfile,
@@ -10,101 +10,45 @@ import '../styles/Profile.css'
 
 const avatarFallbackImage = './avatar-placeholder.png'
 
-export default class Profile extends Component {
-  constructor(props) {
-    super(props)
+export default function Profile(props) {
+  const { userSession, handleSignOut } = props
+  const isLocal = props.match.params.username ? false : true
 
-    this.state = {
-      person: {
-        name() {
-          return 'Anonymous'
-        },
-        avatarUrl() {
-          return avatarFallbackImage
-        },
-      },
-      username: '',
-      newStatus: '',
-      statuses: [],
-      statusIndex: 0,
-      isLoading: false,
-    }
-  }
+  const [statuses, setStatuses] = useState([])
+  const [person, setPerson] = useState({
+    name: () => 'Anonymous',
+    avatarUrl: () => avatarFallbackImage
+  })
+  const [username, setUsername] = useState('')
+  const [newStatus, setNewStatus] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
-  isLocal() {
-    return this.props.match.params.username ? false : true
-  }
+  useEffect(() => {
+    setPerson(new Person(userSession.loadUserData().profile))
+    setUsername(userSession.loadUserData().username)
+  }, [userSession])
 
-  componentWillMount() {
-    const { userSession } = this.props
-    this.setState({
-      person: new Person(userSession.loadUserData().profile),
-      username: userSession.loadUserData().username
-    });
-  }
-
-  componentDidMount() {
-    this.fetchData()
-  }
-
-  handleNewStatusChange(event) {
-    this.setState({ newStatus: event.target.value })
-  }
-
-  handleNewStatusSubmit(event) {
-    this.saveNewStatus(this.state.newStatus)
-    this.setState({
-      newStatus: ""
-    })
-  }
-
-  saveNewStatus(statusText) {
-    const { userSession } = this.props
-    let statuses = this.state.statuses
-
-    let status = {
-      id: this.state.statusIndex++,
-      text: statusText.trim(),
-      created_at: Date.now()
-    }
-
-    statuses.unshift(status)
-    const options = { encrypt: false }
-    userSession.putFile('statuses.json', JSON.stringify(statuses), options)
-      .then(() => {
-        this.setState({
-          statuses: statuses
-        })
-      })
-  }
-
-  fetchData() {
-    const { userSession } = this.props
-    this.setState({ isLoading: true })
-    if (this.isLocal()) {
+  useEffect(() => {
+    setIsLoading(true)
+    if (isLocal) {
       const options = { decrypt: false }
       userSession.getFile('statuses.json', options)
         .then((file) => {
           var statuses = JSON.parse(file || '[]')
-          this.setState({
-            person: new Person(userSession.loadUserData().profile),
-            username: userSession.loadUserData().username,
-            statusIndex: statuses.length,
-            statuses: statuses,
-          })
+          setPerson(new Person(userSession.loadUserData().profile))
+          setUsername(userSession.loadUserData().username)
+          setStatuses(statuses)
         })
         .finally(() => {
-          this.setState({ isLoading: false })
+          setIsLoading(false)
         })
     } else {
-      const username = this.props.match.params.username
+      const username = props.match.params.username
 
       lookupProfile(username)
         .then((profile) => {
-          this.setState({
-            person: new Person(profile),
-            username: username
-          })
+          setPerson(new Person(profile))
+          setUsername(username)
         })
         .catch((error) => {
           console.log('could not resolve profile')
@@ -114,88 +58,104 @@ export default class Profile extends Component {
       userSession.getFile('statuses.json', options)
         .then((file) => {
           var statuses = JSON.parse(file || '[]')
-          this.setState({
-            statusIndex: statuses.length,
-            statuses: statuses
-          })
+          setStatuses(statuses)
         })
         .catch((error) => {
           console.log('could not fetch statuses')
         })
         .finally(() => {
-          this.setState({ isLoading: false })
+          setIsLoading(false)
         })
     }
+  }, [isLocal, props.match.params.username, userSession])
+
+  const handleNewStatusSubmit = (event) => {
+    saveNewStatus(newStatus)
+    setNewStatus('')
   }
 
-  render() {
-    const { handleSignOut, userSession } = this.props;
-    const { person } = this.state;
-    const { username } = this.state;
+  const handleNewStatusChange = (event) => {
+    setNewStatus(event.target.value)
+  }
 
-    return (
-      !userSession.isSignInPending() && person ?
-        <div className="container">
-          <NavBar username={username} user={person} signOut={this.props.handleSignOut} />
-          <div className="row">
-            <div className="col-md-offset-3 col-md-6">
-              <div className="col-md-12">
-                <div className="avatar-section">
-                  <img
-                    src={person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage}
-                    className="img-rounded avatar"
-                    id="avatar-image"
-                    alt="foto de perfil"
-                  />
-                  <div className="username">
-                    <h1>
-                      <span id="heading-name">{person.name() ? person.name()
-                        : 'Nameless Person'}</span>
-                    </h1>
-                    <span>{username}</span>
-                    {this.isLocal() &&
-                      <span>
-                        &nbsp;|&nbsp;
-                      <a onClick={handleSignOut.bind(this)}>(Logout)</a>
-                      </span>
-                    }
-                  </div>
-                </div>
-              </div>
-              {this.isLocal() &&
-                <div className="new-status">
-                  <div className="col-md-12">
-                    <textarea className="input-status"
-                      value={this.state.newStatus}
-                      onChange={e => this.handleNewStatusChange(e)}
-                      placeholder="What's on your mind?"
-                    />
-                  </div>
-                  <div className="col-md-12 text-right">
+  const saveNewStatus = (statusText) => {
+    let status = {
+      id: statuses.length + 1,
+      text: statusText.trim(),
+      created_at: Date.now()
+    }
+
+    statuses.unshift(status)
+    const options = { encrypt: false }
+    userSession.putFile('statuses.json', JSON.stringify(statuses), options)
+      .then(() => setStatuses(statuses))
+  }
+
+  return (
+    !userSession.isSignInPending() && person ?
+      <div className="container">
+        <NavBar username={username} user={person} signOut={handleSignOut} />
+        <div className="row">
+          <div className="col-md-offset-3 col-md-6">
+            <div className="col-md-12">
+              <div className="avatar-section">
+                <img
+                  src={person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage}
+                  className="img-rounded avatar"
+                  id="avatar-image"
+                  alt="foto de perfil"
+                />
+                <div className="username">
+                  <h1>
+                    <span id="heading-name">{person.name ? person.name
+                      : 'Nameless Person'}</span>
+                  </h1>
+                  <span>{username}</span>
+                  {isLocal &&
+                    <span>
+                      &nbsp;|&nbsp;
                     <button
-                      className="btn btn-primary btn-lg"
-                      onClick={e => this.handleNewStatusSubmit(e)}
-                    >
-                      Submit
-                  </button>
-                  </div>
+                        className="link-button"
+                        onClick={handleSignOut} >
+                        (Sair)
+                    </button>
+                    </span>
+                  }
                 </div>
-              }
-              <div className="col-md-12 statuses">
-                {this.state.isLoading && <span>Loading...</span>}
-                {this.state.statuses.map((status) => (
-                  <div className="status" key={status.id}>
-                    {status.text}
-                  </div>
-                )
-                )}
               </div>
             </div>
+            {isLocal &&
+              <div className="new-status">
+                <div className="col-md-12">
+                  <textarea className="input-status"
+                    value={newStatus}
+                    onChange={e => handleNewStatusChange(e)}
+                    placeholder="What's on your mind?"
+                  />
+                </div>
+                <div className="col-md-12 text-right">
+                  <button
+                    className="btn btn-primary btn-lg"
+                    onClick={e => handleNewStatusSubmit(e)}
+                  >
+                    Submit
+                </button>
+                </div>
+              </div>
+            }
+            <div className="col-md-12 statuses">
+              {isLoading && <span>Loading...</span>}
+              {statuses.map((status) => (
+                <div className="status" key={status.id}>
+                  {status.text}
+                </div>
+              )
+              )}
+            </div>
           </div>
-        </div> : null
-    );
-  }
-
+        </div>
+      </div> : null
+  );
 }
 
 Profile.defaultProps = {
