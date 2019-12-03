df859347bec76ef882b381dc7d8fe37932055884
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2019-12-03 11:05:31 -0300

    create grid list

--- src/components/products/index.tsx
+++ src/components/products/index.tsx
@@ -2,8 +2,36 @@ import React, { useState, useEffect } from 'react'
 import { UserSession } from 'blockstack';
 import { Product } from '../../models/Product';
 import PreviewProduct from './_show';
+import {
+  createStyles,
+  GridList,
+  makeStyles,
+  Theme,
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+      overflow: 'hidden',
+      backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+      width: 500,
+      height: 450,
+      alignSelf: 'center',
+    },
+    icon: {
+      color: 'rgba(255, 255, 255, 0.54)',
+    },
+  }),
+);
 
 export default function ListProducts(props: { userSession: UserSession }) {
+  const classes = useStyles();
+
   const [products, setProducts] = useState<Array<Product>>([])
 
   useEffect(() => {
@@ -19,10 +47,12 @@ export default function ListProducts(props: { userSession: UserSession }) {
   }, [props.userSession])
 
   return (
-    <div>
-      {products.map((product: Product) => (
-        <PreviewProduct product={product} />
-      ))}
+    <div className={classes.root}>
+      <GridList cellHeight={160} className={classes.gridList} cols={3}>
+        {products.map((product: Product) => (
+          <PreviewProduct product={product} />
+        ))}
+      </GridList>
     </div>
   )
 }
