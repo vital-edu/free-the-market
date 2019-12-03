1322598554d78f913aafdada6915d8ecf89187ae
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2019-12-03 13:23:10 -0300

    change navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,33 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import '../styles/NavBar.css'
+import {
+  AccountCircle,
+} from '@material-ui/icons'
+import {
+  AppBar,
+  Button,
+  createStyles,
+  Link,
+  IconButton,
+  makeStyles,
+  Theme,
+  Toolbar,
+  Typography,
+} from '@material-ui/core'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
 
 export default function NavBar(props: {
   username: string,
@@ -9,27 +36,54 @@ export default function NavBar(props: {
 }) {
   const { username, user, signOut } = props
 
-  return (
-    user ? <nav className="navbar navbar-expand-md navbar-dark bg-blue fixed-top">
-      <Link className="navbar-brand" to="/">Free the Market</Link>
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item">
-          <Link className="nav-link" to='/'>{username}</Link>
-        </li>
-      </ul>
-      <img
-        src={user.avatarUrl() ? user.avatarUrl() : './avatar-placeholder.png'}
-        className="avatar"
-        width="25"
-        height="25"
-        alt=""
-      />
-      <button
-        className="btn btn-primary"
-        onClick={signOut}
-      >Sair
-      </button>
+  const classes = useStyles();
 
-    </nav> : null
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    window.location.href = 'profile'
+  };
+
+  const handleHomeButton = () => {
+    console.log(window.location.pathname)
+    if (window.location.pathname !== '/') {
+      window.location.href = '/'
+    }
+  }
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            <Button
+              onClick={handleHomeButton}
+              color="inherit">
+              Free the Market
+              </Button>
+          </Typography>
+          {user && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                {user.avatarUrl() ?
+                  <img
+                    src={user.avatarUrl() ? user.avatarUrl() : './avatar-placeholder.png'}
+                    className="avatar"
+                    width="25"
+                    height="25"
+                    alt=""
+                  />
+                  : <AccountCircle />
+                }
+              </IconButton>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
   )
 }
